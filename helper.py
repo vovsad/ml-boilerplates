a3f81b3aaee5a7f2772ac845a55082c0b5cc3f85
volodymyr.sadovyy <volodymyr.sadovyy@gmail.com>
2024-02-27 20:25:55 +0200

    helper first set

---
+++ helper.py
@@ -0,0 +1,105 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt 
+
+# Note: The following confusion matrix code is a remix of Scikit-Learn's
+# plot_confusion_matrix function - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.plot_confusion_matrix.html
+# and Made with ML's introductory notebook - https://github.com/GokuMohandas/MadeWithML/blob/main/notebooks/08_Neural_Networks.ipynb
+import itertools
+from sklearn.metrics import confusion_matrix
+
+def conf_matrix_vis(y_test, y_preds):
+    figsize = (8, 8)
+    
+    # Create the confusion matrix
+    cm = confusion_matrix(y_test, tf.round(y_preds))
+    cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis] # normalize it
+    n_classes = cm.shape[0]
+    
+    # Let's prettify it
+    fig, ax = plt.subplots(figsize=figsize)
+    # Create a matrix plot
+    cax = ax.matshow(cm, cmap=plt.cm.Blues) # https://matplotlib.org/3.2.0/api/_as_gen/matplotlib.axes.Axes.matshow.html
+    fig.colorbar(cax)
+    
+    # Create classes
+    classes = False
+    
+    if classes:
+      labels = classes
+    else:
+      labels = np.arange(cm.shape[0])
+    
+    # Label the axes
+    ax.set(title="Confusion Matrix",
+           xlabel="Predicted label",
+           ylabel="True label",
+           xticks=np.arange(n_classes),
+           yticks=np.arange(n_classes),
+           xticklabels=labels,
+           yticklabels=labels)
+    
+    # Set x-axis labels to bottom
+    ax.xaxis.set_label_position("bottom")
+    ax.xaxis.tick_bottom()
+    
+    # Adjust label size
+    ax.xaxis.label.set_size(20)
+    ax.yaxis.label.set_size(20)
+    ax.title.set_size(20)
+    
+    # Set threshold for different colors
+    threshold = (cm.max() + cm.min()) / 2.
+    
+    # Plot the text on each cell
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+      plt.text(j, i, f"{cm[i, j]} ({cm_norm[i, j]*100:.1f}%)",
+               horizontalalignment="center",
+               color="white" if cm[i, j] > threshold else "black",
+               size=15)
+      
+import numpy as np
+
+def plot_decision_boundary(model, X, y):
+  """
+  Plots the decision boundary created by a model predicting on X.
+  This function has been adapted from two phenomenal resources:
+   1. CS231n - https://cs231n.github.io/neural-networks-case-study/
+   2. Made with ML basics - https://github.com/GokuMohandas/MadeWithML/blob/main/notebooks/08_Neural_Networks.ipynb
+  """
+  # Define the axis boundaries of the plot and create a meshgrid
+  x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
+  y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
+  xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
+                       np.linspace(y_min, y_max, 100))
+
+  # Create X values (we're going to predict on all of these)
+  x_in = np.c_[xx.ravel(), yy.ravel()] # stack 2D arrays together: https://numpy.org/devdocs/reference/generated/numpy.c_.html
+
+  # Make predictions using the trained model
+  y_pred = model.predict(x_in)
+
+  # Check for multi-class
+  if model.output_shape[-1] > 1: # checks the final dimension of the model's output shape, if this is > (greater than) 1, it's multi-class
+    print("doing multiclass classification...")
+    # We have to reshape our predictions to get them ready for plotting
+    y_pred = np.argmax(y_pred, axis=1).reshape(xx.shape)
+  else:
+    print("doing binary classifcation...")
+    y_pred = np.round(np.max(y_pred, axis=1)).reshape(xx.shape)
+
+  # Plot decision boundary
+  plt.contourf(xx, yy, y_pred, cmap=plt.cm.RdYlBu, alpha=0.7)
+  plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.RdYlBu)
+  plt.xlim(xx.min(), xx.max())
+  plt.ylim(yy.min(), yy.max())
+
+def unzip_data(filename):
+  """
+  Unzips filename into the current working directory.
+
+  Args:
+    filename (str): a filepath to a target zip folder to be unzipped.
+  """
+  zip_ref = zipfile.ZipFile(filename, "r")
+  zip_ref.extractall()
+  zip_ref.close()
