6899b55aeccc0a39a7d55cbd8181fa46fcecaf91
vovsad <vovsad@users.noreply.github.com>
2024-03-05 19:14:24 +0200

    added yolo split

--- helper.py
+++ helper.py
@@ -109,3 +109,45 @@ def unzip_data(filename):
   zip_ref.extractall()
   zip_ref.close()
 
+
+import glob
+import random
+import os
+import shutil
+def split_for_yolo(path, labels='labels/', images='images/'):
+# Get all paths to your images files and text files
+img_paths = glob.glob(path + images + '*.jpg')
+txt_paths = glob.glob(path + labels + '*.txt')
+
+# Calculate number of files for training, validation
+data_size = len(img_paths)
+r = 0.8
+train_size = int(data_size * 0.8)
+
+# Shuffle two list
+img_txt = list(zip(img_paths, txt_paths))
+random.seed(43)
+random.shuffle(img_txt)
+img_paths, txt_paths = zip(*img_txt)
+
+# Now split them
+train_img_paths = img_paths[:train_size]
+train_txt_paths = txt_paths[:train_size]
+
+valid_img_paths = img_paths[train_size:]
+valid_txt_paths = txt_paths[train_size:]
+
+# Move them to train, valid folders
+train_folder = PATH+'train/' 
+valid_folder = PATH+'valid/'
+os.mkdir(train_folder)
+os.mkdir(valid_folder)
+
+def move(paths, folder):
+    for p in paths:
+        shutil.move(p, folder)
+
+move(train_img_paths, train_folder)
+move(train_txt_paths, train_folder)
+move(valid_img_paths, valid_folder)
+move(valid_txt_paths, valid_folder)
